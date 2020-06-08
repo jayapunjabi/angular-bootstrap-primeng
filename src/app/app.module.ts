371b81fb94d022ae9a92a98453fad0e2f2788e65
jayapunjabi <jayapunjabi707@gmail.com>
2020-06-09 01:08:58 +0530

    UI and Routing completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { NgModule, APP_BOOTSTRAP_LISTENER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { ContactComponent } from './contact/contact.component';
+import { BodyComponent } from './body/body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    FeaturesComponent,
+    ContactComponent,
+    BodyComponent
+    
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
     // NgbModule.forRoot(),
     // NgbModule.forRoot()
   
